95fd18aa3f244af5ce8757dc19d4bc3c7b31fd65
Anthony Clark <clark.anthony.g@gmail.com>
2015-12-17 19:26:38 -0500

    Typos and small changes

--- Makefile
+++ Makefile
@@ -2,16 +2,16 @@
 all: ct_syslog rt_syslog ct_stdio rt_stdio
 
 
-ct_syslog:
+ct_syslog: test.cpp
 	clang++ --std=c++11 -ggdb3 *.cpp -DLOGGING_IDENTIFIER="test" -DLOGGING_SYSLOG -DLOGGING_COMPILE_TIME -lpthread -o $@
 
-rt_syslog:
+rt_syslog: test.cpp
 	clang++ --std=c++11 -ggdb3 *.cpp -DLOGGING_IDENTIFIER="test" -DLOGGING_SYSLOG -DLOGGING_RUNTIME -lpthread -o $@
 
-ct_stdio:
+ct_stdio: test.cpp
 	clang++ --std=c++11 -ggdb3 *.cpp -DCOLORED_LOGGING -DLOGGING_STDIO -DLOGGING_COMPILE_TIME -o $@
 
-rt_stdio:
+rt_stdio: test.cpp
 	clang++ --std=c++11 -ggdb3 *.cpp -DCOLORED_LOGGING -DLOGGING_STDIO -DLOGGING_RUNTIME -o $@
 
 clean:
--- test.cpp
+++ test.cpp
@@ -8,11 +8,16 @@ int main(void)
     logging::init_logging();
 
     write_log<CRIT>("Log level = %d", log_level);
-    write_log<INFO>("%s test", "test");
+    write_log<INFO>("%s test", "INFO");
+    write_log<WARNING>("%s test", "WARNING");
+    write_log<DEBUG>("%s test", "DEBUG");
 
     set_logging_level(WARNING);
     
     write_log<CRIT>("Log level = %d", log_level);
-    write_log<INFO>("You shouldnt see this");
-
+    write_log<INFO>("%s test", "INFO");
+    write_log<WARNING>("%s test", "WARNING");
+    write_log<DEBUG>("%s test", "DEBUG");
+    
+    write_log<INFO>("You shouldnt see this unless you're a compile time (ct) implementation");
 }
